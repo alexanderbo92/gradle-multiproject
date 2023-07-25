20d66eb75566e58f4030646372e1d8fb72305c14
alexanderbo92 <edwinbohorquez99@gmail.com>
2023-07-25 01:09:03 -0500

    test

---
+++ app/src/main/java/app/TestController.java
@@ -0,0 +1,22 @@
+package app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class TestController {
+
+  @RequestMapping("")
+  public String irPaginaBienvenida() {
+    System.out.println("bienvenido");
+    return "bienvenido";
+  }
+
+  @RequestMapping("/health")
+  public ResponseEntity<?> health() {
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
---
+++ base/src/main/java/base/MyBase.java
@@ -0,0 +1,25 @@
+package base;
+
+import common.MyLibrary;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.stat.Frequency;
+
+@Slf4j
+public abstract class MyBase implements MyInterface {
+
+  @Override
+  public void doSomeProcess() {
+
+    MyLibrary.doSomeProcess();
+    //   log.info("log floor: {}", LongMath.log2(8, RoundingMode.FLOOR));
+  }
+
+  public void countStringFrequencies() {
+    Frequency f = new Frequency();
+    f.addValue("one");
+    f.addValue("One");
+    f.addValue("oNe");
+    f.addValue("Z");
+    // log.info("f.getCumPct('Z'): {}", f.getCumPct("Z")); // displays 0.5
+  }
+}
