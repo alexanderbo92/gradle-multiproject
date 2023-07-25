8fbfbd1d4f9b3ad3eb7eff8f4a4cee30df47852a
alexanderbo92 <edwinbohorquez99@gmail.com>
2023-07-25 18:41:55 -0500

    test message

--- app/src/main/java/app/controller/TestController.java
+++ app/src/main/java/app/controller/TestController.java
@@ -1,23 +1,24 @@
-package app;
+package app.controller;
 
+import base.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/test")
 public class TestController {
 
   @RequestMapping("")
   public String irPaginaBienvenida() {
-    System.out.println("bienvenido");
+    System.out.println("Saludo:  " + Message.message());
     return "bienvenido";
   }
 
   @RequestMapping("/health")
   public ResponseEntity<?> health() {
-    System.out.println("bienvenido");
+    System.out.println("health");
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
