c03e194233bcc968901b89ffd947f6c2f5913281
alexanderbo92 <edwinbohorquez99@gmail.com>
2023-07-25 21:37:52 -0500

    java test 3

--- base/src/main/java/base/MyBase.java
+++ base/src/main/java/base/MyBase.java
@@ -1,10 +1,8 @@
 package base;
 
 import common.MyLibrary;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.math3.stat.Frequency;
 
-@Slf4j
 public abstract class MyBase implements MyInterface {
 
   @Override
--- common/src/main/java/common/MyLibrary.java
+++ common/src/main/java/common/MyLibrary.java
@@ -1,8 +1,5 @@
 package common;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class MyLibrary {
   public static void doSomeProcess() {
     // log.info("do some process");
